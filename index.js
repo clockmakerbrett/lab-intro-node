6c426cbc0fd9e52711267e7bfc0252788731c73b
Brett Sherwood <clockmakerbrett@gmail.com>
2020-07-20 10:44:38 -0400

    done

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
+  max() {
+    if (this.items[0]) {
+      return this.items.reduce((accumulator, item) => {
+        return Math.max(accumulator, item);
+      });
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  min() {
+    if (this.items[0]) {
+      return this.items.reduce((accumulator, item) => {
+        return Math.min(accumulator, item);
+      });
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
   sum() {}
-
   avg() {}
 }
 
